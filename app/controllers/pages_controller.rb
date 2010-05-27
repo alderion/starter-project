79b8f8ba042e9c1e27ceb041bc5bdd7ae4a21978
Peter Abplanalp <pabplanalp@gmail.com>
2010-05-27 09:13:36 -0600

    added static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def about
+    @title = "About"
+  end
+
+  def contact
+    @title = "Contact"
+  end
+
+  def home
+    @title = "Home"
+  end
+
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the correct title" do
+      get 'about'
+      response.should have_tag("title", "About")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the correct title" do
+      get 'contact'
+      response.should have_tag("title", "Contact")
+    end
+  end
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have the correct title" do
+      get 'home'
+      response.should have_tag("title", "Home")
+    end
+  end
+end
